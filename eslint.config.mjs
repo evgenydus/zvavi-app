b4d00a19bc91a7a60755c63f9d02ce532ecdc2b9
Evgeny Dus <evgeny21dus@gmail.com>
2024-11-10 22:17:44 +0400

    Initial setup (#1)
    * feat: add prettier pre-commit and eslint rules
    
    * chore: update eslintrc and lint
    
    * chore: configure husky
    
    * chore: update deps

---
+++ eslint.config.mjs
@@ -0,0 +1,124 @@
+// eslint-disable max-lines
+import prettier from 'eslint-plugin-prettier'
+import sortKeysFix from 'eslint-plugin-sort-keys-fix'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  allConfig: js.configs.all,
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+})
+
+export default [
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:tailwindcss/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:tailwindcss/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ),
+  {
+    plugins: {
+      prettier,
+      'sort-keys-fix': sortKeysFix,
+    },
+
+    rules: {
+      '@typescript-eslint/no-var-requires': 'off',
+      'import/extensions': 'off',
+      'import/no-cycle': 'off',
+      'import/order': 'off',
+      'import/prefer-default-export': 'off',
+
+      'max-lines': [
+        'error',
+        {
+          max: 100,
+          skipBlankLines: true,
+          skipComments: true,
+        },
+      ],
+
+      'no-param-reassign': 'off',
+      'no-underscore-dangle': 'off',
+
+      'padding-line-between-statements': [
+        'error',
+        {
+          blankLine: 'always',
+          next: '*',
+          prev: ['block-like', 'const', 'let'],
+        },
+        {
+          blankLine: 'any',
+          next: ['const', 'let'],
+          prev: ['const', 'let'],
+        },
+        {
+          blankLine: 'always',
+          next: ['block-like', 'return'],
+          prev: '*',
+        },
+      ],
+
+      'prettier/prettier': 'error',
+      'react/forbid-prop-types': 'off',
+
+      'react/function-component-definition': [
+        2,
+        {
+          namedComponents: 'arrow-function',
+        },
+      ],
+
+      'react/jsx-filename-extension': [
+        1,
+        {
+          extensions: ['.tsx'],
+        },
+      ],
+
+      'react/jsx-props-no-spreading': [
+        2,
+        {
+          html: 'ignore',
+        },
+      ],
+
+      'react/jsx-sort-props': [
+        'error',
+        {
+          reservedFirst: ['key', 'ref'],
+        },
+      ],
+
+      'react/jsx-uses-react': 'off',
+
+      'react/no-multi-comp': [
+        'error',
+        {
+          ignoreStateless: false,
+        },
+      ],
+
+      'react/react-in-jsx-scope': 'off',
+      'react/require-default-props': 'off',
+      semi: ['error', 'never'],
+      'sort-keys': ['error', 'asc'],
+      'sort-keys-fix/sort-keys-fix': 'error',
+      'tailwindcss/no-custom-classname': 'off',
+    },
+  },
+]
