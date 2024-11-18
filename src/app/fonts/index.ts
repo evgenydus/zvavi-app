b45d5e03b4b720a90e605f085c8cfbd3dcf8b2a6
Evgeny Dus <evgeny21dus@gmail.com>
2024-11-18 19:53:16 +0400

    feat: implement very basic header (#5)
    * feat: implement very basic header
    
    * refactor: CR

---
+++ src/app/fonts/index.ts
@@ -0,0 +1,7 @@
+import { Ubuntu_Sans_Mono as UbuntuSansMono } from 'next/font/google'
+
+export const ubuntuSans = UbuntuSansMono({
+  display: 'swap',
+  subsets: ['latin'],
+  variable: '--font-ubuntu-sans',
+})
