70191c3029cac7e30ad27e985d1460a82d41e940
syudintsev@slb.com <Sergei Iudintsev>
2025-07-10 17:25:49 +0400

    added confirmation modal component

---
+++ src/app/UI/components/ConfirmationModal/ConfirmationModal.tsx
@@ -0,0 +1,35 @@
+import { useTranslations } from 'next-intl'
+
+import { Button } from '@/UI/components/inputs'
+import type { ModalProps } from '@/UI/components/Modal'
+import { Modal, ModalBody, ModalFooter } from '@/UI/components/Modal'
+
+export type ConfirmationModalProps = ModalProps & {
+  onConfirm: () => void
+  onCancel?: () => void
+}
+
+const ConfirmationModal = ({
+  children,
+  isOpen,
+  onCancel,
+  onClose,
+  onConfirm,
+  title,
+}: ConfirmationModalProps) => {
+  const tActions = useTranslations('common.actions')
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} title={title}>
+      {children}
+      <ModalFooter>
+        <Button onClick={onCancel || onClose}>{tActions('cancel')}</Button>
+        <Button className="ml-auto" onClick={onConfirm}>
+          {tActions('confirm')}
+        </Button>
+      </ModalFooter>
+    </Modal>
+  )
+}
+
+export default ConfirmationModal
