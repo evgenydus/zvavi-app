0dc00a2030c02c96499f1353f1d04b320d3471e2
syudintsev@slb.com <Sergei Iudintsev>
2025-07-10 14:21:49 +0400

    fixed tailwind classes order

--- src/app/UI/components/Modal/ModalFooter/ModalFooter.tsx
+++ src/app/UI/components/Modal/ModalFooter/ModalFooter.tsx
@@ -3,9 +3,7 @@ type ModalFooterProps = {
 }
 
 const ModalFooter = ({ children }: ModalFooterProps) => (
-  <footer className="border-t px-4 lg:px-6 py-2 lg:py-4 flex gap-2 lg:gap-4">
-    {children}
-  </footer>
+  <footer className="flex gap-2 border-t px-4 py-2 lg:gap-4 lg:px-6 lg:py-4">{children}</footer>
 )
 
 export default ModalFooter
