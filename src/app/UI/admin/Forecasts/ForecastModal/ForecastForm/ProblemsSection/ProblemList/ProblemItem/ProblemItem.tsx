3e4e34c93f9b38e4e586226999de5794d3e9987c
syudintsev@slb.com <Sergei Iudintsev>
2025-07-11 15:52:35 +0400

    added problem deletion confirmation dialog

--- src/app/UI/admin/Forecasts/ForecastModal/ForecastForm/ProblemsSection/ProblemList/ProblemItem/ProblemItem.tsx
+++ src/app/UI/admin/Forecasts/ForecastModal/ForecastForm/ProblemsSection/ProblemList/ProblemItem/ProblemItem.tsx
@@ -1,6 +1,7 @@
-import { useCallback } from 'react'
+import { useCallback, useState } from 'react'
 import { useTranslations } from 'next-intl'
 
+import { ConfirmationModal } from '@/UI/components/ConfirmationModal'
 import Properties from './Properties'
 import { ActionButtons, Aspects } from '../../../common/listItem'
 
@@ -14,7 +15,12 @@ type ProblemItemProps = {
 }
 
 const ProblemItem = ({ canEdit, onDelete, onEdit, problemData }: ProblemItemProps) => {
+  const tActions = useTranslations('common.actions')
   const tForm = useTranslations('admin.forecast.form')
+  const tProblemTypes = useTranslations('admin.forecast.form.problems.options.problemType')
+  const [isDeleteConfirmationModalOpen, setIsDeleteConfirmationModalOpen] = useState(false)
+
+  const { description, type: problemType } = problemData
 
   const handleDelete = useCallback(() => {
     onDelete(problemData.id!)
@@ -24,30 +30,44 @@ const ProblemItem = ({ canEdit, onDelete, onEdit, problemData }: ProblemItemProp
     onEdit(problemData.id!)
   }, [onEdit, problemData])
 
-  const { description } = problemData
+  const openDeleteConfirmationModal = () => setIsDeleteConfirmationModalOpen(true)
+
+  const closeDeleteConfirmationModal = () => setIsDeleteConfirmationModalOpen(false)
 
   return (
-    <div className="w-full rounded bg-black/[0.03] p-3">
-      <div className="mb-3 flex items-center justify-between">
-        <h3 className="text-xl font-semibold">
-          {tForm(`problems.options.problemType.${problemData.type}`)}
-        </h3>
+    <>
+      <div className="w-full rounded bg-black/[0.03] p-3">
+        <div className="mb-3 flex items-center justify-between">
+          <h3 className="text-xl font-semibold">{tProblemTypes(problemType)}</h3>
 
-        <ActionButtons canEdit={canEdit} onDelete={handleDelete} onEdit={handleEdit} />
-      </div>
-
-      <div className="mb-6 flex items-center justify-between gap-6">
-        <Properties problemData={problemData} />
-        <Aspects className="w-[355px]" item={problemData} />
-      </div>
-
-      {description && (
-        <div>
-          <h4 className="mb-2 font-semibold">{tForm('common.labels.description')}:</h4>
-          <p className="max-h-28 overflow-y-auto text-justify">{description}</p>
+          <ActionButtons
+            canEdit={canEdit}
+            onDelete={openDeleteConfirmationModal}
+            onEdit={handleEdit}
+          />
         </div>
-      )}
-    </div>
+
+        <div className="mb-6 flex items-center justify-between gap-6">
+          <Properties problemData={problemData} />
+          <Aspects className="w-[355px]" item={problemData} />
+        </div>
+
+        {description && (
+          <div>
+            <h4 className="mb-2 font-semibold">{tForm('common.labels.description')}:</h4>
+            <p className="max-h-28 overflow-y-auto text-justify">{description}</p>
+          </div>
+        )}
+      </div>
+
+      <ConfirmationModal
+        isOpen={isDeleteConfirmationModalOpen}
+        onClose={closeDeleteConfirmationModal}
+        onConfirm={handleDelete}
+        title={`${tActions('delete')} ${tProblemTypes(problemType)}?`}
+        variant="delete"
+      />
+    </>
   )
 }
 
