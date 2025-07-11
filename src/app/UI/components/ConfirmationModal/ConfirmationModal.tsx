290a34a0162e04514aa83e5c1715a71ecb78c97e
syudintsev@slb.com <Sergei Iudintsev>
2025-07-11 13:58:53 +0400

    added confirmation modal variants

--- src/app/UI/components/ConfirmationModal/ConfirmationModal.tsx
+++ src/app/UI/components/ConfirmationModal/ConfirmationModal.tsx
@@ -2,31 +2,36 @@ import { useTranslations } from 'next-intl'
 
 import { Button } from '@/UI/components/inputs'
 import type { ModalProps } from '@/UI/components/Modal'
-import { Modal, ModalFooter } from '@/UI/components/Modal'
+import { Modal, ModalBody, ModalFooter } from '@/UI/components/Modal'
 
-export type ConfirmationModalProps = ModalProps & {
+export type ConfirmationModalProps = Omit<ModalProps, 'children'> & {
   onConfirm: () => void
   onCancel?: () => void
+  variant?: 'confirm' | 'delete'
+  description?: string
 }
 
 const ConfirmationModal = ({
-  children,
+  description,
   isOpen,
   onCancel,
   onClose,
   onConfirm,
   title,
+  variant = 'confirm',
 }: ConfirmationModalProps) => {
   const tActions = useTranslations('common.actions')
+  const tModal = useTranslations(`modal.${variant}`)
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose} title={title}>
-      {children}
+    <Modal isOpen={isOpen} onClose={onClose} title={title || tModal(`title`)}>
+      <ModalBody>{description || tModal('description')}</ModalBody>
       <ModalFooter>
-        <Button onClick={onCancel || onClose}>{tActions('cancel')}</Button>
-        <Button className="ml-auto" onClick={onConfirm}>
-          {tActions('confirm')}
+        {/* TODO: Use proper colored buttons once they're available - <link to a task> */}
+        <Button className="ml-auto" onClick={onCancel || onClose}>
+          {tActions('cancel')}
         </Button>
+        <Button onClick={onConfirm}>{tActions(variant)}</Button>
       </ModalFooter>
     </Modal>
   )
