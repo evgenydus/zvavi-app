08085f08f2a70adb59c79c02819407ea556e0c22
syudintsev@slb.com <Sergei Iudintsev>
2025-07-16 16:10:36 +0400

    improved loader component

--- src/app/UI/loader/Loader.tsx
+++ src/app/UI/loader/Loader.tsx
@@ -6,87 +6,33 @@ export type LoaderProps = {
 
 const Loader = ({ size = 100 }: LoaderProps) => {
   const circleRadius = size / 2.4
-  const circleLength = 2 * Math.PI * circleRadius
   const circleCenter = size / 2
-  const strokedArcLength = circleLength * 0.5
+  const strokedArcLength = 2 * Math.PI * circleRadius * 0.6
   const strokeWidth = (size - circleRadius * 2) / 2
 
   return (
-    <div className="fixed left-0 top-0 size-full content-center bg-black/30 backdrop-blur-sm">
+    <div className="fixed left-0 top-0 z-10 size-full content-center bg-black/30 backdrop-blur-sm">
       <svg
-        className="m-auto animate-spin"
+        className="m-auto animate-spin text-primary"
         fill="none"
         height={size}
         viewBox={`0 0 ${size} ${size}`}
         width={size}
         xmlns="http://www.w3.org/2000/svg"
       >
+        <defs>
+          <linearGradient id="grad1">
+            <stop offset="0%" stopColor="currentColor" stopOpacity="1" />
+            <stop offset="100%" stopColor="currentColor" stopOpacity="0" />
+          </linearGradient>
+        </defs>
+
         <circle
-          className="stroke-primary"
           cx={circleCenter}
           cy={circleCenter}
           r={circleRadius}
+          stroke="url(#grad1)"
           strokeDasharray={strokedArcLength}
-          strokeLinecap="round"
-          strokeWidth={strokeWidth}
-        />
-        <circle
-          className="stroke-primary opacity-60"
-          cx={circleCenter}
-          cy={circleCenter}
-          r={circleRadius}
-          strokeDasharray={strokedArcLength}
-          strokeDashoffset={circleLength * 0.1}
-          strokeLinecap="round"
-          strokeWidth={strokeWidth}
-        />
-        <circle
-          className="stroke-primary opacity-50"
-          cx={circleCenter}
-          cy={circleCenter}
-          r={circleRadius}
-          strokeDasharray={strokedArcLength}
-          strokeDashoffset={circleLength * 0.15}
-          strokeLinecap="round"
-          strokeWidth={strokeWidth}
-        />
-        <circle
-          className="stroke-primary opacity-40"
-          cx={circleCenter}
-          cy={circleCenter}
-          r={circleRadius}
-          strokeDasharray={strokedArcLength}
-          strokeDashoffset={circleLength * 0.2}
-          strokeLinecap="round"
-          strokeWidth={strokeWidth}
-        />
-        <circle
-          className="stroke-primary opacity-30"
-          cx={circleCenter}
-          cy={circleCenter}
-          r={circleRadius}
-          strokeDasharray={strokedArcLength}
-          strokeDashoffset={circleLength * 0.25}
-          strokeLinecap="round"
-          strokeWidth={strokeWidth}
-        />
-        <circle
-          className="stroke-primary opacity-20"
-          cx={circleCenter}
-          cy={circleCenter}
-          r={circleRadius}
-          strokeDasharray={strokedArcLength}
-          strokeDashoffset={circleLength * 0.3}
-          strokeLinecap="round"
-          strokeWidth={strokeWidth}
-        />
-        <circle
-          className="stroke-primary opacity-10"
-          cx={circleCenter}
-          cy={circleCenter}
-          r={circleRadius}
-          strokeDasharray={strokedArcLength}
-          strokeDashoffset={circleLength * 0.35}
           strokeLinecap="round"
           strokeWidth={strokeWidth}
         />
