15bb974dafb6f4c79b76750b773d23f069e5b1e9
evgenydus <evgeny21dus@gmail.com>
2025-10-13 18:59:57 +0400

    chore: lint

--- src/app/data/hooks/forecasts/useForecastStatusToggle.ts
+++ src/app/data/hooks/forecasts/useForecastStatusToggle.ts
@@ -7,7 +7,10 @@ import type { ForecastStatusToggleVariables } from './types'
 import { supabase } from '@/data'
 import { forecastsKeys } from '@/data/query-keys'
 
-const toggleStatus = async ({ forecastId, status }: ForecastStatusToggleVariables): Promise<void> => {
+const toggleStatus = async ({
+  forecastId,
+  status,
+}: ForecastStatusToggleVariables): Promise<void> => {
   const { error } = await supabase.from('forecasts').update({ status }).eq('id', forecastId)
 
   handleSupabaseError(error)
