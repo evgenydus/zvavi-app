679f75210b197f91e4d317faf1a2932271383cb1
evgenydus <evgeny21dus@gmail.com>
2025-10-19 23:49:51 +0400

    refactor: move traslations from json to yaml

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,5 +1,9 @@
 import { notFound } from 'next/navigation'
+import type { AbstractIntlMessages } from 'next-intl'
 import { getRequestConfig } from 'next-intl/server'
+import fs from 'node:fs'
+import path from 'node:path'
+import YAML from 'yaml'
 
 import { routing } from './routing'
 import { type Locale, locales } from '../config'
@@ -7,14 +11,36 @@ import { type Locale, locales } from '../config'
 export default getRequestConfig(async ({ requestLocale }) => {
   let locale = await requestLocale
 
-  if (!locale || !routing.locales.includes(locale as never)) {
+  if (!locale || !routing.locales.includes(locale as Locale)) {
     locale = routing.defaultLocale
   }
 
   if (!locales.includes(locale as Locale)) return notFound()
 
+  const messagesDir = path.join(process.cwd(), 'messages', locale)
+
+  if (!fs.existsSync(messagesDir)) return notFound()
+
+  const files = fs.readdirSync(messagesDir).filter((file) => file.endsWith('.yml'))
+
+  if (files.length === 0) return notFound()
+
+  let messages: AbstractIntlMessages = {}
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const fileName of files) {
+    const filePath = path.join(messagesDir, fileName)
+    const content = fs.readFileSync(filePath, 'utf8')
+    const parsed = YAML.parse(content)
+
+    messages = {
+      ...messages,
+      ...parsed, // можно сделать deepMerge, если нужно
+    }
+  }
+
   return {
-    messages: (await import(`../../messages/${locale}.json`)).default,
+    messages,
     timeZone: 'Asia/Tbilisi',
   }
 })
