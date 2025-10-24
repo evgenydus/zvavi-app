a6dff1fdb4032fbe35816b7cc572efd941c2b467
evgenydus <evgeny21dus@gmail.com>
2025-10-24 21:21:43 +0400

    feat: implement About Us page content

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,11 +1,40 @@
 import { useTranslations } from 'next-intl'
 
+import { HTMLContainer } from '@/UI/components'
+
 import { PageWrapper } from '@/UI/containers/PageWrapper'
 
 const Page = () => {
   const t = useTranslations()
 
-  return <PageWrapper title={t('navigation.about')}>About Us Page</PageWrapper>
+  return (
+    <PageWrapper title={t('navigation.about')}>
+      <div className="space-y-3 text-justify">
+        <HTMLContainer
+          component="p"
+          content={t.rich('about.content.paragraph1', {
+            b: (chunks) => `<b>${chunks}</b>`,
+          })}
+        />
+
+        <p>{t('about.content.paragraph2')}</p>
+
+        <HTMLContainer
+          component="p"
+          content={t.rich('about.content.paragraph3', {
+            strong: (chunks) => `<strong>${chunks}</strong>`,
+          })}
+        />
+
+        <HTMLContainer
+          component="p"
+          content={t.rich('about.content.paragraph4', {
+            strong: (chunks) => `<strong>${chunks}</strong>`,
+          })}
+        />
+      </div>
+    </PageWrapper>
+  )
 }
 
 export default Page
