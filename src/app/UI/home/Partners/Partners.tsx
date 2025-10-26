ceb1f0b91704067e2478adb1257de19112d11608
evgenydus <evgeny21dus@gmail.com>
2025-10-26 21:53:26 +0400

    feat: add button to partners section

--- src/app/UI/home/Partners/Partners.tsx
+++ src/app/UI/home/Partners/Partners.tsx
@@ -1,23 +1,25 @@
 import { useTranslations } from 'next-intl'
 
 import { partners } from '@/data/constants/partners'
+import { routes } from '@/UI/header/NavMenu/constants'
 
-import { PageSection } from '@/UI/components'
-
-import PartnerBadge from '@/UI/home/Partners/PartnerBadge'
+import { ButtonLink, PageSection } from '@/UI/components'
+import PartnerBadge from './PartnerBadge'
 
 const Partners = () => {
   const t = useTranslations()
 
   return (
     <PageSection title={t('partners.main.title')}>
-      <div className="flex gap-2 overflow-x-auto scrollbar-hide">
+      <div className="mb-4 flex gap-2 overflow-x-auto scrollbar-hide">
         {partners.map((partner) => {
           if (partner.isHidden) return null
 
           return <PartnerBadge key={partner.name} partner={partner} />
         })}
       </div>
+
+      <ButtonLink href={routes.partners}>{t('partners.main.seeAllButton')}</ButtonLink>
     </PageSection>
   )
 }
